2dd7378c52e66582b05b17e337727f1813080225
netgodRUS <netgod@bk.ru>
2022-11-20 18:50:22 +0300

    Initial commit

---
+++ src/main/java/Yo/project/SpringDemoBot/service/TelegramBot.java
@@ -0,0 +1,57 @@
+package Yo.project.SpringDemoBot.service;
+
+import Yo.project.SpringDemoBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+    public TelegramBot (BotConfig config){
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                        startCommandRecieved(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:  sendMessage(chatId, "Sorry, command is not recognised");
+                }
+            }
+    }
+
+    private void startCommandRecieved(long chatId, String name)  {
+        String answer = "Hi, "+name + ", nice to meet you";
+        sendMessage(chatId, answer);
+
+    }
+    private void sendMessage (long chatId, String textToSend)  {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e){
+
+        }
+    }
+}
